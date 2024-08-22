87ad1a95fe9807bd81bc85debf555e8d83f93c56
Guri <khalsagudeep@gmail.com>
2024-08-22 19:39:28 +0530

    update: height fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-full">
+    <main className="flex min-h-[100%] flex-col items-center justify-between w-full">
       <LazyLeafletMap center={[0, 0]} zoom={3}>
         <LazyMarkerCluster>
           <></>
